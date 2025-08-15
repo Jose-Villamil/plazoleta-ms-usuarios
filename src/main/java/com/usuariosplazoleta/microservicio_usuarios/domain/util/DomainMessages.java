067ae7bf84262ccf99bb7fe5f92a2543b1fc1652
Jose-Villamil <jose.villamil@pragma.com.co>
2025-08-15 17:56:20 -0500

    Refactorización de código para mantener los mensajes del dominio estandar

--- src/main/java/com/usuariosplazoleta/microservicio_usuarios/domain/util/DomainMessages.java
+++ src/main/java/com/usuariosplazoleta/microservicio_usuarios/domain/util/DomainMessages.java
@@ -4,19 +4,8 @@ public final class DomainMessages {
 
     private DomainMessages() {}
 
-    public static final String NAME_REQUIRED = "El campo Nombre no puede ser nulo o vacío";
-    public static final String LAST_REQUIRED = "El campo Apellido no puede ser nulo o vacío";
-    public static final String DOCUMENT_REQUIRED = "El campo Documento de identidad no puede ser nulo o vacío";
-    public static final String NUMBER_PHONE_REQUIRED = "El campo Celular no puede ser nulo o vacío";
-    public static final String BIRTH_DATE_REQUIRED = "El campo Fecha de nacimiento no puede ser nulo o vacío";
+    public static final String FIELD_REQUIRED = "El campo %s no puede ser nulo o vacío";
+    public static final String FIELD_INVALID = "%s inválido";
     public static final String USER_LEGAL_AGE = "El usuario debe ser mayor de edad";
-    public static final String EMAIL_REQUIRED = "El campo Correo no puede ser nulo o vacío";
-    public static final String PASSWORD_REQUIRED = "El campo Clave no puede ser nulo o vacío";
-
-    //public static final String USER_DOESNOT_HAVE_ROL = "El usuario no tiene el rol: ";
-
-    public static final String INVALID_DOCUMENT = "Documento inválido";
-    public static final String INVALID_PHONE = "Celular inválido";
-    public static final String INVALID_EMAIL = "Correo invalido";
 
 }
